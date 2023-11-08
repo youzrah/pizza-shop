fc10c852ac3b5582c37801be5da0eb74c9333d56
youzrah <yoshieee19@gmail.com>
2023-11-08 18:33:06 +0800

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Dashboard from './Components/Admin/Dashboard';
+import ProductCreate from './Components/Admin/ProductCreate';
+import { toast, ToastContainer } from 'react-toastify';
 
 function App() {
   return (
@@ -8,8 +10,26 @@ function App() {
       <Router>
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
+          <Route path='/product/create' element={<ProductCreate />} />
         </Routes>
       </Router>
+
+
+
+
+
+      <ToastContainer
+        position="top-right"
+        autoClose={4000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
     </div>
   );
 }
