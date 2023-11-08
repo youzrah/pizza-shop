68913b536fbf8db6836c236de521cc3330e63ef0
youzrah <yoshieee19@gmail.com>
2023-11-08 23:44:47 +0800

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,17 +5,31 @@ import ProductCreate from './Components/Admin/ProductCreate';
 import { ToastContainer } from 'react-toastify';
 import ProductsList from './Components/Admin/ProductsList';
 import ProductUpdate from './Components/Admin/ProductUpdate';
+import Home from './Components/Home';
+import Header from './Components/Layout/Header'
+import Footer from './Components/Layout/Footer'
+import Login from './Components/User/Login';
+import Register from './Components/User/Register';
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
+
+          {/* User Access */}
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
+          {/* Admin Access */}
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path='/products/list' element={<ProductsList />} />
           <Route path='/product/create' element={<ProductCreate />} />
           <Route path='/product/update/:productId' element={<ProductUpdate />} />
         </Routes>
+        <Footer />
       </Router>
 
 
