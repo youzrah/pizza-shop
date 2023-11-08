7bb62c92794301b96a68cdf1a6865ac48b8fee2b
youzrah <yoshieee19@gmail.com>
2023-11-08 18:32:40 +0800

    add react toastify

---
+++ frontend/src/Components/Layout/Toast.js
@@ -0,0 +1,42 @@
+import { toast, ToastContainer } from 'react-toastify';
+
+export const success = (message) => {
+    toast.success(message, {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+}
+
+export const error = (message) => {
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+}
+
+export const warning = (message) => {
+    toast.warn(message, {
+        position: "top-right",
+        autoClose: 4000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+    });
+}
+
+// export { success, error, warning }
